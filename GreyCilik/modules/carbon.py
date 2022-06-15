098957463464b2269fba1bbd2a58e7d6b9e2cb3d
Arsyabot <95653453+Arsyabot@users.noreply.github.com>
2022-06-15 12:24:34 +0700

    Update carbon.py

--- GreyCilik/modules/carbon.py
+++ GreyCilik/modules/carbon.py
@@ -24,14 +24,14 @@ async def carbon_func(_, message):
     carbon.close()
 
 
-MEMEK = "https://telegra.ph/file/a807460d64ac73c5dc83a.jpg"
+MEMEK = "https://telegra.ph/file/d1e8cc958d662150b36ad.jpg"
 
 @pbot.on_message(filters.command("repo"))
 async def repo(_, message):
     await message.reply_photo(
         photo=MEMEK,
-        caption=f"""✨ **Hey I'm Cilik Robot** ✨ 
-**Owner repo : [Grey](https://t.me/greyyvbss)**
+        caption=f"""✨ **Hey I'm ALBY Robot** ✨ 
+**Owner repo : [Grey](https://t.me/Punya_Alby)**
 **Python Version :** `{y()}`
 **Library Version :** `{o}`
 **Telethon Version :** `{s}`
@@ -42,9 +42,9 @@ async def repo(_, message):
             [
                 [
                     InlineKeyboardButton(
-                        "Repo", url="https://github.com/grey423/GCilik"), 
+                        "Repo", url="https://github.com/"), 
                     InlineKeyboardButton(
-                        "Support", url="https://t.me/CilikSupport")
+                        "Support", url="https://t.me/ruangdiskusikami")
                 ]
             ]
         )
