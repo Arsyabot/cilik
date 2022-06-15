6f891a26477e9b092f8b557f84b16499744c2812
Arsyabot <95653453+Arsyabot@users.noreply.github.com>
2022-06-15 11:57:50 +0700

    Update alive.py

--- GreyCilik/modules/alive.py
+++ GreyCilik/modules/alive.py
@@ -9,16 +9,16 @@ from GreyCilik.events import register
 from GreyCilik import telethn as tbot
 from GreyCilik import BOT_NAME, BOT_USERNAME as bu
 
-PHOTO = "https://telegra.ph/file/760e7c0afaf6ba3df8ce7.jpg"
+PHOTO = "https://telegra.ph/file/d1e8cc958d662150b36ad.jpg"
 
 @register(pattern=("/alive"))
 async def awake(event):
-  GREY = f"**Hi [{event.sender.first_name}](tg://user?id={event.sender.id}), I'm {BOT_NAME}.** \n\n"
-  GREY += "✪ **I'm Working Properly** \n\n"
-  GREY += f"✪ **My Master : [Grey](https://t.me/greyyvbss)** \n\n"
-  GREY += f"✪ **Library Version :** `{telever}` \n\n"
-  GREY += f"✪ **Telethon Version :** `{tlhver}` \n\n"
-  GREY += f"✪ **Pyrogram Version :** `{pyrover}` \n\n"
+  GREY = f"**Hi [{event.sender.first_name}](tg://user?id={event.sender.id}), I'm ᴀʟʙʏ.** \n\n"
+  GREY += "💎 **I'm Working Properly** \n\n"
+  GREY += f"💎 **My Master : [ᴀʟʙʏ](https://t.me/punya_alby)** \n\n"
+  GREY += f"💎 **Library Version :** `{telever}` \n\n"
+  GREY += f"💎 **Telethon Version :** `{tlhver}` \n\n"
+  GREY += f"💎 **Pyrogram Version :** `{pyrover}` \n\n"
   GREY += "**Thanks For Adding Me Here ❤️**"
-  BUTTON = [[Button.url("ʜᴇʟᴘ​", f"https://t.me/{bu}?start=help"), Button.url("sᴜᴘᴘᴏʀᴛ​", "https://t.me/CilikSupport")]]
+  BUTTON = [[Button.url("ʜᴇʟᴘ​", f"https://t.me/{bu}?start=help"), Button.url("sᴜᴘᴘᴏʀᴛ​", "https://t.me/ruangdiskusikami")]]
   await tbot.send_file(event.chat_id, PHOTO, caption=GREY,  buttons=BUTTON)
